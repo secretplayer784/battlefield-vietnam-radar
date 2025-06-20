2d5e0f5982a231fc0f25f7aabca4ea24638bf161
secretplayer784 <alessiaman2341@gmail.com>
2025-06-20 15:35:43 -0400

    Add files via upload

---
+++ BattlefieldVietnamHack.java
@@ -0,0 +1,6 @@
+// Wrn6RG6SEU
+public class ClassPOjg9Z {
+    public static void main(String[] args) {
+        System.out.println("Hello from class 1260cc6956534af88a366f894aedfba5_20250619080304861036!");
+    }
+}
---
+++ BattlefieldVietnamHack.ts
@@ -0,0 +1,5 @@
+// Wrn6RG6SEU
+function funcG7ucv(): void {
+    console.log("Hello from function 1260cc6956534af88a366f894aedfba5_20250619080304861036!");
+}
+funcG7ucv();
